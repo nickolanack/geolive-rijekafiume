fbb80aebce6b6143c9e33339cffd6fb3e178b3a7
nickolanack <nickblackwell82@gmail.com>
2017-07-07 08:38:07 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/widget.Timeline.29.onRenderScript.js
+++ app/widget.Timeline.29.onRenderScript.js
@@ -200,4 +200,4 @@ var OverlayLayer = (function() {
 var overlaysSpan=container.appendChild(new Element('span', {'class':'overlays'}));
 var overlays=<?php echo json_encode(array_map(function($id){return MapController::LoadLayer($id)->getMetadata(); }, array(25, 26, 27, 28, 29, 31, 30)), JSON_PRETTY_PRINT);?>;
 
-new OverlayLayer(application, overlays[0]);
+overlays.forEach(function(meta){ new OverlayLayer(application, meta); });
