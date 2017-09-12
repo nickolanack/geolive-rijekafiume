dd63bd9068c41c0f48e2ea4c42cbc68695488ff8
nickolanack <nickblackwell82@gmail.com>
2017-09-12 14:49:39 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Polygon Overlays.content.1.script.js
+++ app/Polygon Overlays.content.1.script.js
@@ -2,7 +2,7 @@
 <?php  
 
 if(GetClient()->isAdmin()){
-    $linkIcons=GetWidget('polygon-config')->modalViewParams('form')['url'];
+    $link=GetWidget('polygon-config')->modalViewParams('form')['url'];
     ?>
     var link=<?php echo json_encode($link);?>;
  
