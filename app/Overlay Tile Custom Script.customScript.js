6ece292f6ed3be9c4727c0179e9828802becf736
nickolanack <nickblackwell82@gmail.com>
2017-09-12 15:47:21 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Overlay Tile Custom Script.customScript.js
+++ app/Overlay Tile Custom Script.customScript.js
@@ -6,7 +6,18 @@
 			//	'xtile' => $xtile,
 			//	'ytile' => $ytile,
 			//	'zoom' => $zoom,
-			$featureMedatadata['']='';
+			// 'layer'=>$layer
+// 			"polyStyle": {
+//                 "color": "7f659ab3"
+//             },
+//             "lineStyle": {
+//                 "color": "ff659ab3",
+//                 "width": 1
+//             },
+			
+			
+			$featureMedatadata['polyStyle']['color']='7f699ab3';
+			$featureMedatadata['lineStyle']['color']='ff699ab3';
 			SpatialRenderer::RenderFeatureOnTile($featureMetadata, $image, $bounds);
 			
 ?>
