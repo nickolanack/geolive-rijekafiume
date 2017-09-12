6e60e75cb7f0dd85ec6f1670cb5fab5a6fbe8d5c
nickolanack <nickblackwell82@gmail.com>
2017-09-12 16:08:31 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Overlay Tile Custom Script.customScript.js
+++ app/Overlay Tile Custom Script.customScript.js
@@ -14,11 +14,12 @@
 //                 "color": "ff659ab3",
 //                 "width": 1
 //             },
-			
+				
+			Emit('onCustomRender', $featureMetadata);
 			
 			$featureMedatadata['polyStyle']['color']='7f00ff00';
 			$featureMedatadata['lineStyle']['color']='ff699ab3';
-			Emit('onCustomRender', $featureMetadata);
+		
 			SpatialRenderer::RenderFeatureOnTile($featureMetadata, $image, $bounds);
 			
 ?>
