274c85ca1934b6941c2fafc5e9730595cd221d7a
nickolanack <nickblackwell82@gmail.com>
2017-09-12 22:50:25 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Overlay Tile Custom Script.customScript.js
+++ app/Overlay Tile Custom Script.customScript.js
@@ -14,11 +14,28 @@
 //                 "color": "ff659ab3",
 //                 "width": 1
 //             },
+
+            $polyconfig=GetWidget('polygon-config');
+            $kmlcolor='#7f000000';
+            $kmlline='#ff000000';
+            $configcolor=$polyconfig->getParameter('layer-'.$layer.'-color', 'rgb(0,0,0)');
+                if(!empty($configcolor)){
+                    $parts=explode(',', $configcolor);
+                    $rgb=array_map(function($str){
+                        return intval(preg_replace("/[^0-9]/", "", $str));
+                    }, $parts);
+                    
+                    $kmlcolor = "#5F".sprintf("%02x%02x%02x",  $rgb[2],  $rgb[1],  $rgb[0]);
+                    $kmlline="#7f".sprintf("%02x%02x%02x",  $rgb[2],  $rgb[1],  $rgb[0]);
+                }
+                
+               
+
 				
 			GetLogger('layertile')->info('custom render', $featureMetadata);
 			
-			$featureMetadata['polyStyle']['color']='7f00ff00';
-			$featureMetadata['lineStyle']['color']='ff699ab3';
+			$featureMetadata['polyStyle']['color']=$kmlcolor;
+			$featureMetadata['lineStyle']['color']=$kmlline;
 		
 			SpatialRenderer::RenderFeatureOnTile($featureMetadata, $image, $bounds);
 			
