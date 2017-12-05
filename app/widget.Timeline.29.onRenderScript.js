49e9feac570f0f125ac14cdf050b2eca3d8d0c0c
nickolanack <nickblackwell82@gmail.com>
2017-12-05 10:15:06 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/widget.Timeline.29.onRenderScript.js
+++ app/widget.Timeline.29.onRenderScript.js
@@ -5,39 +5,41 @@
 
 //TODO: this cannot be added in english mode
 
-//TODO: this cannot be added in english mode
-if(Cookie.read('lang')=='lang-hr'){
-	AddLocalization({
-		"layer_id_0":"Komercijalne aktivnosti i industrija",
-		"layer_id_1":"Uprava i vojne aktivnosti",
-		"layer_id_2":"Obrazovanje i kultura",
-		"layer_id_3":"Stanovanje",
-		"layer_id_4":"Javna mjesta i spomenici",
-
-
-		"pin_1804":"Austrijska uprava",
-		"pin_1822":"Mađarska uprava",
-		"pin_1848":"Privremena hrvatska uprava",
-		"pin_1868":"Hrvatsko-ugarska nagodba",
-		"pin_1919":"Mirovni sporazum u St. Germainu",
-		"pin_1920":"Rapalski ugovor",
-		"pin_1924":"Rimski ugovor",
-		"pin_april_1941_tip":"Travanj 1941",
-		"pin_april_1941":"Talijanska okupacija Sušaka",
-		"pin_1943":"Kapitulacija Italije i njemačka okupacija Rijeke",
-		"pin_may_1945_tip":"Svibanj 1945",
-		"pin_may_1945":"Jugoslavensko zauzeće Rijeke",
-		"pin_1947":"Pariški mirovni ugovor",
-
-
-		"era_1719_1914":"Habsburška Monarhija",
-		"era_1914_1924":"Prvi svjetski rat",
-		"era_1924_1941":"Talijanska uprava",
-		"era_1941_1947":"Drugi svjetski rat",
-		"era_1947_1990":"Jugoslavenska uprava",
-		"era_1990_2015":"Hrvatska uprava"
-
-	});
+var ln=Cookie.read('lang');
+if(ln=='lang-hr'||ln=="lang-it"){
+	AddLocalization(({
+	    'lang-hr':{
+    
+    
+    		"pin_1719":"Slobodna Luka",
+    		"pin_1822":"Mađarska uprava",
+    		"pin_1848":"Privremena hrvatska uprava",
+    		"pin_1868":"Hrvatsko-ugarska nagodba",
+    		"pin_1919":"Mirovni sporazum u St. Germainu",
+    		"pin_1920":"Rapalski ugovor",
+    		"pin_1924":"Rimski ugovor",
+    		"pin_april_1941_tip":"Travanj 1941",
+    		"pin_april_1941":"Talijanska okupacija Sušaka",
+    		"pin_1943":"Kapitulacija Italije i njemačka okupacija Rijeke",
+    		"pin_may_1945_tip":"Svibanj 1945",
+    		"pin_may_1945":"Jugoslavensko zauzeće Rijeke",
+    		"pin_1947":"Pariški mirovni ugovor",
+    
+    
+    		"era_1719_1914":"Habsburška Monarhija",
+    		"era_1914_1924":"Prvi svjetski rat",
+    		"era_1924_1941":"Talijanska uprava",
+    		"era_1941_1947":"Drugi svjetski rat",
+    		"era_1947_1990":"Jugoslavenska uprava",
+    		"era_1990_2015":"Hrvatska uprava"
+    
+        },
+        'lang-it':{
+            "pin_1719":"Porto Franco",
+            
+        }
+	    
+	})[ln]);
 }
 
 var erraSpan=container.appendChild(new Element('span', {'class':'era-s'}));
