19fc1f1fa1f4fb9cc13acad14a4227ec813db716
nickolanack <nickblackwell82@gmail.com>
2019-03-29 15:17:18 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/CSV Sync.customScript.js
+++ app/CSV Sync.customScript.js
@@ -2,9 +2,9 @@ $vars=array_merge(
     get_object_vars($json), array('internal'=>'data')
     );
     
-if(!key_exists('data', $vars)){
-    return array("success"=>false, "message"=>'Expected edit data in json');
-}
+// if(!key_exists('data', $vars)){
+//     return array("success"=>false, "message"=>'Expected edit data in json');
+// }
 
 
 GetPlugin('Email')->getMailerWithTemplate('syncSheet', $vars)
