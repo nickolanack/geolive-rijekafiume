a14289297560315b275fe162b469c7276d55b80d
nickolanack <nickblackwell82@gmail.com>
2019-04-02 14:40:19 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/CSV Sync.customScript.js
+++ app/CSV Sync.customScript.js
@@ -1,62 +1,68 @@
-$vars=array_merge(
-    get_object_vars($json), array('internal'=>'data')
-    );
-    
-// if(!key_exists('data', $vars)){
-//     return array("success"=>false, "message"=>'Expected edit data in json');
-// }
+class CsvSync {
 
+	public function execute($json) {
 
-GetPlugin('Email')->getMailerWithTemplate('syncSheet', $vars)
-		->to('nickblackwell82@gmail.com')
-		->send();
+		$vars = array_merge(
+			get_object_vars($json), array('internal' => 'data')
+		);
 
+		// if(!key_exists('data', $vars)){
+		//     return array("success"=>false, "message"=>'Expected edit data in json');
+		// }
 
-if(!(key_exists('secret', $json)&&$json->secret===GetWidget('csvSyncConfig')->getParameter('csvSecret'))){
-     return array("success"=>false, "message"=>'missing secret');
+		GetPlugin('Email')->getMailerWithTemplate('syncSheet', $vars)
+			->to('nickblackwell82@gmail.com')
+			->send();
+
+		if (!(key_exists('secret', $json) && $json->secret === GetWidget('csvSyncConfig')->getParameter('csvSecret'))) {
+			return array("success" => false, "message" => 'missing secret');
+		}
+
+		if (!key_exists('id', $json)) {
+			return array("success" => false, "message" => 'missing id');
+		}
+
+		if (!key_exists('type', $json)) {
+			return array("success" => false, "message" => 'type');
+		}
+
+		if ($json->type !== "marker") {
+			return array("success" => false, "message" => 'Expected type to be marker: ' . $json->type);
+		}
+
+		$updateInfo = array();
+
+		GetPlugin('Maps');
+		try {
+			$feature = (new \spatial\FeatureLoader())->fromId((int) $json->id);
+		} catch (Exception $e) {
+			return array("success" => false, "message" => 'Item ' . ((int) $json->id) . ': not found');
+		}
+
+		if (key_exists('title', $json)) {
+			$feature->setName($json->title);
+		}
+		if (key_exists('description', $json)) {
+			$feature->setDescription($json->title);
+		}
+
+		GetPlugin('Attributes');
+		$attributes = (new \attributes\Record('markerAttributes'))->getValues((int) $json->id, $json->type);
+
+		if (key_exists('attributes', $json) && key_exists('markerAttributes', $json->attributes)) {
+			$attributes = array_merge($attributes, get_object_vars($json->attributes->markerAttributes));
+		}
+
+		return array(
+			'marker' => $feature->getMetadata(),
+			'attributes' => array("markerAttributes" => $attributes),
+		);
+
+	}
+
 }
 
-if(!key_exists('id', $json)){
-    return array("success"=>false, "message"=>'missing id');
-}
-
-if(!key_exists('type', $json)){
-    return array("success"=>false, "message"=>'type');
-}
-
-if($json->type!=="marker"){
-    return array("success"=>false, "message"=>'Expected type to be marker: '.$json->type);
-}
-
-
-$updateInfo=array();
-
-GetPlugin('Maps');
-try {
-    $feature = (new \spatial\FeatureLoader())->fromId((int) $json->id);
-} catch (Exception $e) {
-    return array("success"=>false, "message"=>'Item '.((int) $json->id).': not found');
-}
-
-if(key_exists('title', $json)){
-    $feature->setName($json->title);
-}
-if(key_exists('description', $json)){
-    $feature->setDescription($json->title);
-}
-
-GetPlugin('Attributes');
-$attributes=(new \attributes\Record('markerAttributes'))->getValues((int) $json->id, $json->type);
-
-if(key_exists('attributes', $json)&&key_exists('markerAttributes', $json->attributes)){
-    $attributes=array_merge($attributes,  get_object_vars($json->attributes->markerAttributes));
-}
 
 
 
-return array(
-    'marker'=>$feature->getMetadata(),
-    'attributes'=>array("markerAttributes"=>$attributes)
-    );
-
-//return array("success"=>false, "message"=>'Invalid field: '.$json->field.': Expected title or description');
+return (new CsvSync())->execute($json);
