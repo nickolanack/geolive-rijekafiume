15903ca4ced56c6fa95aeaadc40975d303523d91
nickolanack <nickblackwell82@gmail.com>
2019-04-11 12:32:02 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/mobile-app-config.formatOutputScript.js
+++ app/mobile-app-config.formatOutputScript.js
@@ -7,7 +7,7 @@ foreach($categories as $cat){
             
             "label"=>$cat,
             "action"=> "form",
-            "view"=> "map",
+            "view"=> "mainmap",
              "icon"=> "{categoriesIcon}"
         );
     }
