94384ead882d84ad8221926c48bd34e6882f8dc8
nickolanack <nickblackwell82@gmail.com>
2019-04-11 13:30:18 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/widget.AttributesCSVExport.74.filterFieldsScript.js
+++ app/widget.AttributesCSVExport.74.filterFieldsScript.js
@@ -1,1 +1,13 @@
+if(strpos($fieldFormat['label'] 'item owner')===0){
+    return false;
+}
+
+if(strpos($fieldFormat['label'] 'layer ')===0){
+    return false;
+}
+
+if(in_array(array('coordinates', 'image count', 'url',	'created date',	'last modification date', 'read access'), $fieldFormat['label'])){
+    return false;
+}
+
 return strpos($fieldFormat['field'], 'attributes.markerAttributes')===false;
