04e829295310cb588fc83d3a8476ad13e5666b37
nickolanack <nickblackwell82@gmail.com>
2019-04-12 15:12:42 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/widget.AjaxMethod.78.customScript.js
+++ app/widget.AjaxMethod.78.customScript.js
@@ -5,14 +5,21 @@ $attr=new \attributes\Record('curatedAttributes');
 
 //$layer=(int)$json->layer;
 
+error_log(json_encode($json));
+
 return array(
-    "items"=>(new spatial\Features())->listLayerItems(36)
+    "items"=>array_filter((new spatial\Features())->listLayerItems(36)
         ->map(function($item)use($attr){
             
            
             $item['attributes']=$attr->getValues($item['id'], $item['type']);
             
             return $item;
+            
+        }), function($item)use($json){
+                
+                
+            return true;
             
         })
     );
