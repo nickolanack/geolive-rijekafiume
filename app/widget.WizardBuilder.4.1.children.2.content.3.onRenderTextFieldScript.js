4e438f9e168640c4cea95b38b01499ef1e036b5f
nickolanack <nickblackwell82@gmail.com>
2019-05-06 11:30:37 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

---
+++ app/widget.WizardBuilder.4.1.children.2.content.3.onRenderTextFieldScript.js
@@ -0,0 +1,18 @@
+var element=inputElement;
+            
+            try{
+				element.type='date';
+			}catch(e){
+
+			}
+			function supportsDate() {
+			    var input = document.createElement('input');
+			    input.setAttribute('type','date');
+			    var notADateValue = 'not-a-date';
+			    input.setAttribute('value', notADateValue); 
+			    return (input.value !== notADateValue);
+			}
+
+			if(supportsDate()){
+				return;
+			}
