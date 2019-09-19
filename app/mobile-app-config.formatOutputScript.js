4ae3bba6384da2bd4cb9d3f8a75e1fcaa3cc796e
nickolanack <nickblackwell82@gmail.com>
2019-09-19 10:40:54 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/mobile-app-config.formatOutputScript.js
+++ app/mobile-app-config.formatOutputScript.js
@@ -1,74 +1,70 @@
 GetPlugin('Attributes');
 
+$makeFeildsetButtonset = function ($categoryName, $template) {
 
-$makeFeildsetButtonset=function($categoryName, $template){
+	$categories = (new \attributes\Record('curatedAttributes'))->distinctValues($categoryName);
 
-    $categories=(new \attributes\Record('curatedAttributes'))->distinctValues($categoryName);
+	$categories = array_filter($categories, function ($c) {
+		return !!$c;
+	});
 
+	$buttons = array();
 
-    $categories=array_filter($categories, function($c){
-        return !!$c;
-    });
+	if (empty($categories)) {
+		$categories = array("Empty list: " . $categoryName);
+	}
 
+	$json = json_encode($template);
+	foreach ($categories as $cat) {
+		if ($cat) {
 
-    $buttons=array();
+			$button = str_replace(json_encode("{value}"), json_encode($cat), $json);
+			$button = str_replace("{Name}", ucfirst($categoryName), $button);
+			$buttons[] = json_decode($button);
+		}
+	}
 
-    if(empty($categories)){
-        $categories=array("Empty list: ".$categoryName);
-    }
+	
 
-    $json=json_encode($template);
-    foreach($categories as $cat){
-        if($cat){
+	return array(
 
-            $button=str_replace(json_encode("{value}"), json_encode($cat), $json);
-            $button=str_replace("{Name}", ucfirst($categoryName), $button);
-            $buttons[]=json_decode($button);
-        }
-    }
-
-
-    return array(
-        array(
-            "type"=>"card",
-            "fields"=>array(
-                array(
-                    "type"=>"fieldset",
-                    "fields"=>array(
-                        array(
-                            "type"=>"buttonset",
-                            "className"=> "btn-main",
-                            "buttons"=>$buttons
-                        )
-                    )
-                )
-            )
-        )
-    );
+		"type" => "fieldset",
+		"fields" => $buttons
+	);
 
 };
 
-$template=array(
-        "label"=>"{value}",
-        "action"=> "form",
-        "view"=> "mainmap",
-        "remember"=>false,
-        // "icon"=> "{categoriesIcon}",
-         "data"=> array(
-            "layers"=>array(array(
-                "id"=>36,
-                "filter"=>array("filter{Name}"=>"{value}")
-            ))),
-         
-    );
 
-$parameters['types']=$makeFeildsetButtonset(
-    'category', $template);
 
-$parameters['periods']=$makeFeildsetButtonset(
-    'period', $template);
 
-$parameters['tours']=$makeFeildsetButtonset(
-    'tour', $template);
+$template = array(
+    "type" => "card",
+    "fields" => array(
+        array(
+            "type":"label",
+            "label" => "{value}",
+        )
+    ),
+	
+	"action" => "form",
+	"view" => "mainmap",
+	"remember" => false,
+	// "icon"=> "{categoriesIcon}",
+	"data" => array(
+		"layers" => array(array(
+			"id" => 36,
+			"filter" => array("filter{Name}" => "{value}"),
+		))),
+
+);
+
+$parameters['types'] = $makeFeildsetButtonset(
+	'category', $template);
+
+$parameters['periods'] = $makeFeildsetButtonset(
+	'period', $template);
+
+$parameters['tours'] = $makeFeildsetButtonset(
+	'tour', $template);
 
 return $parameters;
