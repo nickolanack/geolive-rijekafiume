425a43aab163606ff446b8bd2484288b993c28bb
nickolanack <nickblackwell82@gmail.com>
2019-11-21 11:34:56 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/mobile-app-config.formatOutputScript.js
+++ app/mobile-app-config.formatOutputScript.js
@@ -171,7 +171,7 @@ $template = array(
 
 
 $kabobCat=function($category, $template="{value}"){
-	return array_combine($makeFieldObject($category,"{value-kabob}"), array_combine($makeFieldObject($category,"{value}")))
+	return array_combine($makeFieldObject($category,"{value-kabob}"), array_combine($makeFieldObject($category,"{value}")));
 
 };
 
