842853d7c02af9cff54cc071404437d16995fa7e
nickolanack <nickblackwell82@gmail.com>
2021-03-30 11:21:15 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/mobile-url-handler.customScript.js
+++ app/mobile-url-handler.customScript.js
@@ -28,4 +28,4 @@ if(key_exists('HTTP_USER_AGENT', $_SERVER)){
 
 
 
-header("Location: rijekafiumeapp://rijekafiume.geolive.ca/mobile/".$marker);
+header("Location: rijekafiumeapp://rijekafiume.geolive.ca/mobile/".$marker."/asdfasdf");
