490c20a3ebc91f5d8d0bfb69c546111e97e232e0
nickolanack <nickblackwell82@gmail.com>
2021-03-30 09:02:24 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/mobile-url-handler.customScript.js
+++ app/mobile-url-handler.customScript.js
@@ -1,1 +1,10 @@
-print_r($_SERVER['REQUEST_URI']);
+$url=$_SERVER['REQUEST_URI'];
+$parts=explode('/mobile/', $url);
+$marker=array_pop($parts);
+$marker=explode('/', $marker);
+$marker=array_shift($marker);
+$marker=explode('#', $marker);
+$marker=array_shift($marker);
+$marker=intval($marker);
+
+echo $marker;
