4b065b30d86c4389428efcb8b23b07debed72c7e
nickolanack <nickblackwell82@gmail.com>
2021-04-15 08:56:48 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Url redirect 2.customScript.js
+++ app/Url redirect 2.customScript.js
@@ -10,24 +10,3 @@ $marker=intval($marker);
 //echo $marker;
 
 error_log(print_r($_SERVER, true));
-
-if(key_exists('HTTP_REFERER', $_SERVER)){
-    if(strpos($_SERVER['HTTP_REFERER'], 'android-app')===0){
-        //header("Location: https://play.google.com/store/apps/details?id=org.rijekafiume.ca");
-        //return;
-        //header("Location: rijekafiumeapp://rijekafiume.geolive.ca/mobile-2/asdfasdf/".$marker);
-        //return;
-    }
-}
-
-
-if(key_exists('HTTP_USER_AGENT', $_SERVER)){
-    if(stripos($_SERVER['HTTP_USER_AGENT'], 'iphone')!==false){
-        header("Location: https://apps.apple.com/app/rijeka-fiume-in-flux/id1471481859");
-        return;
-    }
-}
-
-
-
-header("Location: rijekafiumeapp://rijekafiume.geolive.ca/mobile/asdfasdf/".$marker);
