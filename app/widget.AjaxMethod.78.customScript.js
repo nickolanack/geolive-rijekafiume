09b9a8567657407b135e01b7fd0c1374c4d09231
nickolanack <nickblackwell82@gmail.com>
2022-03-14 14:06:15 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/widget.AjaxMethod.78.customScript.js
+++ app/widget.AjaxMethod.78.customScript.js
@@ -1,8 +1,8 @@
-$analyticUrl=escapeshellarg('https://analytic.fra.geoforms.ca/index.php?json='.json_encode((object) array(
+$analyticUrl=escapeshellarg('https://analytic.fra.geoforms.ca/index.php?json='.urlencode(json_encode((object) array(
     'ip'=>$_SERVER['REMOTE_ADDR'],
     'request'=>'list-markers',
     'args'=>$json
-    )));
+    ))));
 $cmd  = "curl -g --max-time 60 ";
 $cmd .= $analyticUrl;
 $cmd .= " > /dev/null 2>&1 &";
