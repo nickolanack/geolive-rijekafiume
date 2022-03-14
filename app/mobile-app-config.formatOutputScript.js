2e66b9895ebe6af36ee229bb35e4d782d5cb8449
nickolanack <nickblackwell82@gmail.com>
2022-03-14 12:04:32 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/mobile-app-config.formatOutputScript.js
+++ app/mobile-app-config.formatOutputScript.js
@@ -1,5 +1,5 @@
 $analyticUrl=escapeshellarg('https://analytic.fra.geoforms.ca/index.php?json='.json_encode((object) array()));
-$cmd  = "curl --max-time 60 ";
+$cmd  = "curl -g --max-time 60 ";
 $cmd .= $analyticUrl;
 $cmd .= " > /dev/null 2>&1 &";
 exec($cmd);
