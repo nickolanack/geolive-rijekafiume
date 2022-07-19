23eae610ea3c0b71b26ea4322d9af0f4ab9aabbf
nickolanack <nickblackwell82@gmail.com>
2022-07-19 09:25:43 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/plugin.Maps.12.formatMapitemScript.js
+++ app/plugin.Maps.12.formatMapitemScript.js
@@ -1,3 +1,4 @@
+error_log(print_r($feature, true));
 //$feature->marker->style='https://rijekafiume.geolive.ca/administrator/components/com_geolive/users_files/user_files_680/Uploads/18u_uFD_[G]_vVJ_[ImAgE].png?thumb=%3E48%3E150';
 //die(print_r($feature));
 //return $feature;
