dab9f406f0decad9df4ae0a5fcb45a2f799a5839
nickolanack <nickblackwell82@gmail.com>
2022-07-19 09:12:35 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/plugin.Maps.12.layerDisplayFilterScript.js
+++ app/plugin.Maps.12.layerDisplayFilterScript.js
@@ -1,1 +1,2 @@
+die(print_r($feature));
 $feature->marker->style='https://rijekafiume.geolive.ca/administrator/components/com_geolive/users_files/user_files_680/Uploads/18u_uFD_[G]_vVJ_[ImAgE].png?thumb=%3E48%3E150';
