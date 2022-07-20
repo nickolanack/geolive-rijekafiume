642437b0f05cbff01a1b1144a4326fee71237292
nickolanack <nickblackwell82@gmail.com>
2022-07-20 12:16:34 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Custom Markers- and Login.customScript.js
+++ app/Custom Markers- and Login.customScript.js
@@ -1,1 +1,10 @@
 application.setDefaultLoginView('loginForm');
+
+
+listMarkers
+
+(new AjaxControlQuery(CoreAjaxUrlRoot, 'user_function', {
+					"widget": "listMarkers"
+				})).addEvent('onSuccess', function(resp) {
+				    console.log(resp)
+			}).execute();
