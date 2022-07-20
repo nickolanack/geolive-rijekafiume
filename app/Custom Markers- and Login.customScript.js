799dc8fc33c062d4ec706b9d911d0b6f579d03a0
nickolanack <nickblackwell82@gmail.com>
2022-07-20 12:33:15 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Custom Markers- and Login.customScript.js
+++ app/Custom Markers- and Login.customScript.js
@@ -16,6 +16,19 @@ application.setDefaultLoginView('loginForm');
 	    console.log(item);
 	    if(typeof map[item.getId()]!='undefined'){
 	        console.log(map[item.getId()]);
+	        var data=map[item.getId()];
+	        
+	        var category=data.attributes.category;
+	        category=category.toLowerCase().replace(/[^0-9a-z ]/g, "").split(' ').join('-');
+	        
+	        var period=data.attributes.period[0];
+	        period=period.toLowerCase().replace(/[^0-9a-z ]/g, "").split(' ').join('-'));
+	        
+	        var path='https://rijekafiume.geolive.ca/images/slideshow/markers/';
+	        path+=category+'-'+period'+.png';
+	        
+	        item.setIcon(path);
+	        
 	    }
 	   
 	}
