df70b61e0a26ad2dce73d664691b6206b0d1e303
nickolanack <nickblackwell82@gmail.com>
2022-07-20 12:56:11 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Custom Markers- and Login.customScript.js
+++ app/Custom Markers- and Login.customScript.js
@@ -17,17 +17,20 @@ application.setDefaultLoginView('loginForm');
 	    if(typeof map[item.getId()]!='undefined'){
 	        console.log(map[item.getId()]);
 	        var data=map[item.getId()];
-	        
-	        var category=data.attributes.category;
-	        category=category.toLowerCase().replace(/[^0-9a-z ]/g, "").split(' ').join('-');
-	        
-	        var period=data.attributes.period[0];
-	        period=period.toLowerCase().replace(/[^0-9a-z ]/g, "").split(' ').join('-');
-	        
-	        var path='https://rijekafiume.geolive.ca/images/slideshow/markers/';
-	        path+=category+'-'+period+'.png';
-	        
-	        item.setIcon(path);
+	        try{
+    	        var category=data.attributes.category;
+    	        category=category.toLowerCase().replace(/[^0-9a-z ]/g, "").split(' ').join('-');
+    	        
+    	        var period=data.attributes.period[0];
+    	        period=period.toLowerCase().replace(/[^0-9a-z ]/g, "").split(' ').join('-');
+    	        
+    	        var path='https://rijekafiume.geolive.ca/images/slideshow/markers/';
+    	        path+=category+'-'+period+'.png';
+    	        
+    	        item.setIcon(path);
+	        }catch(e){
+	            item.hide();
+	        }
 	        
 	    }
 	   
